756a38973e6008aee9dbc341d12ad45f85b20e59
youssefazzouz2 <youssefazzouz2@gmail.com>
2023-12-04 13:16:38 +0000

    prosit9

--- src/tn/esprit/gestionzoo/entities/InvalidAgeException.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.gestionzoo.entities;
-//Instruction 34
-public class InvalidAgeException extends Exception {
-    public InvalidAgeException(String message) {
-        super(message);
-    }
-}
